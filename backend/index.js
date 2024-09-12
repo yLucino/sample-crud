12f029613023ab2a91d70d2a2ceb44cffab553d8
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2024-09-12 19:05:51 -0300

    Database integration completed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import userRouter from './routes/users.js';
 import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/', userRouter);
 
 app.listen(8800);
