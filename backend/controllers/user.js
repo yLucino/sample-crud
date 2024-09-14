80c1c17e6705bcc10474a0312c9b7e46b9807fd0
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2024-09-14 11:08:19 -0300

    construction of the POST, PUT, DELETE api and creating the routes to be able to use them

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,3 +9,47 @@ export const getUsers = (_, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const addUser = (req, res) => {
+  const q = "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário criado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q = "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário atualizado com sucesso.");
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM usuarios WHERE `id` = ?";
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário deletado com sucesso.")
+  });
+};
